d07168ffd1e5d462d92416099a07d3b78aecdea5
Nikiforov Max <mv.nikiforov@gmail.com>
2022-01-04 16:35:47 +0300

    add Logger to App

--- src/main/java/mypackage/App.java
+++ src/main/java/mypackage/App.java
@@ -8,6 +8,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        log.info("выполнен вывод в консоль Hello World!")
+
+
         System.out.println("Second string");
         System.out.println("Third string");
     }
