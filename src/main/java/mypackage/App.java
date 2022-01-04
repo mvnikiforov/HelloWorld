f0779c6ccbaa3529f2e531d4964999918d358620
Nikiforov Max <mv.nikiforov@gmail.com>
2022-01-04 20:02:08 +0300

    log4j solve problem

--- src/main/java/mypackage/App.java
+++ src/main/java/mypackage/App.java
@@ -1,16 +1,19 @@
 package mypackage;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
     // инициация логирования
-    private static Logger log = Logger.getLogger(App.class);
+    private static Logger log = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        log.trace("выполнен вывод в консоль Hello World!");
+        log.debug("выполнен вывод в консоль Hello World!");
         log.info("выполнен вывод в консоль Hello World!");
-
-        System.out.println("Second string");
-        System.out.println("Third string");
+        log.warn("выполнен вывод в консоль Hello World!");
+        log.error("выполнен вывод в консоль Hello World!");
+        log.fatal("выполнен вывод в консоль Hello World!");
     }
 }
