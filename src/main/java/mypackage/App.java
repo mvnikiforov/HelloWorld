d563445895a13802809819a6034b0492478f0264
Nikiforov Max <mv.nikiforov@gmail.com>
2022-01-04 18:17:16 +0300

    add info after Hello World output

--- src/main/java/mypackage/App.java
+++ src/main/java/mypackage/App.java
@@ -8,8 +8,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        log.info("выполнен вывод в консоль Hello World!")
-
+        log.info("выполнен вывод в консоль Hello World!");
 
         System.out.println("Second string");
         System.out.println("Third string");
