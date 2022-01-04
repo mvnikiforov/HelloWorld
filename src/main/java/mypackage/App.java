ef979596824a35495edaea93a6c72aeb81e48432
Nikiforov Max <mv.nikiforov@gmail.com>
2022-01-04 16:26:08 +0300

    add Logger

--- src/main/java/mypackage/App.java
+++ src/main/java/mypackage/App.java
@@ -1,6 +1,11 @@
 package mypackage;
 
+import org.apache.log4j.Logger;
+
 public class App {
+    // инициация логирования
+    private static Logger log = Logger.getLogger(App.class);
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Second string");
