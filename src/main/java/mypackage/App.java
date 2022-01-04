60630f69abe51a16e0b7685670518f88c5132931
Nikiforov Max <mv.nikiforov@gmail.com>
2022-01-04 12:23:00 +0300

    second line

---
+++ src/main/java/mypackage/App.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Second string");
+    }
+}
